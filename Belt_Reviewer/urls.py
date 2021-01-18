5d1b82937a9994054f1b91c14ddd35a408d798f5
Erin Winking <ewink@yahoo.com>
2021-01-17 20:31:24 -0800

    BOOK REVIEW and ADD BOOK pages templated.

--- Belt_Reviewer/urls.py
+++ Belt_Reviewer/urls.py
@@ -3,4 +3,6 @@ from . import views
 	
 urlpatterns = [
 		path('', views.home, name = 'home'),
+        path('books/', views.book_reviews, name = 'book_reviews'),
+        path('books/add_book/', views.add_book_review, name = 'add_book_review'),
 	]
--- Belt_Reviewer/views.py
+++ Belt_Reviewer/views.py
@@ -5,3 +5,15 @@ def home(request):
         'page_title': 'DojoReads || Login or Register!'
     }
     return render(request, 'html/register_and_login.html', context)
+
+def book_reviews(request):
+    context = {
+        'page_title': 'Recent Book Reviews!'
+    }
+    return render(request, 'html/book_reviews.html', context)
+
+def add_book_review(request):
+    context = {
+        'page_title': 'Add A Book & Review!'
+    }
+    return render(request, 'html/new_book.html', context)
